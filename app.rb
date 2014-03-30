2e2a0017250b460c0cb88b8863e05f101bfe426b
Ellie Schneiders <ems2141@gmail.com>
2014-03-30 16:25:21 -0600

    user can create a new task and add it to the homepage.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
 
 class App<Sinatra::Application
+  NEW_TASK =""
 
   get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/' do
+    NEW_TASK=params['new_task']
     erb :index
   end
 
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,9 +5,15 @@ require_relative '../app'
 Capybara.app = App
 
 feature "User can create a task and add it to the homepage" do
-  scenario "User can view welcome on the homepage" do
+  scenario "User can create a new task" do
     visit '/'
     expect(page).to have_content "Welcome"
+
+    click_on "Add a Task"
+    fill_in 'new_task', with: "laundry"
+    click_on "Create Task"
+
+    expect(page).to have_content "laundry"
   end
 end
 
